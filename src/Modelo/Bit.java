f5ab942f5b784ce35d0f96524b7cf7d5d4643931
Marco Antonio Adarme Jaimes <madarme@ufps.edu.co>
2020-09-24 22:34:00 -0500

    Clases iniciales del proyecto

---
+++ src/Modelo/Bit.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author madar
+ */
+public class Bit {
+    
+    private boolean valor;
+
+    public Bit(boolean valor) {
+        this.valor = valor;
+    }
+
+    public Bit() {
+    }
+
+    public boolean isValor() {
+        return valor;
+    }
+
+    public void setValor(boolean valor) {
+        this.valor = valor;
+    }
+    
+    
+    
+}
---
+++ src/Negocio/SopaBinaria.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+import Modelo.Bit;
+
+/**
+ *
+ * @author madar
+ */
+public class SopaBinaria {
+    
+    private Bit mySopaBinaria[][];
+
+    public SopaBinaria() {
+    }
+    
+     public SopaBinaria(String rutaArchivoExcel) {
+    }
+    
+    
+    public int getCuantasVeces_Horizontal(int decimal)
+    {
+        return 0;
+    }
+    
+    
+    public int getCuantasVeces_Vertical(int decimal)
+    {
+        return 0;
+    }
+    
+    
+    
+    public int getCuantasVeces_Diagonal(int decimal)
+    {
+        return 0;
+    }
+}
