e57f949f386ec0f10c855edf5201a9d980e18838
jeison ferrer ortega <jeisonomarfort@ufps.edu.co>
2020-09-26 21:33:36 -0500

    Se agregó metodo que convierte el entero en un Array de objetos tipo Bits

--- src/Negocio/SopaBinaria.java
+++ src/Negocio/SopaBinaria.java
@@ -19,11 +19,32 @@ public class SopaBinaria {
     }
     
      public SopaBinaria(String rutaArchivoExcel) {
+         
     }
+    /**
+     * Metodo que convierte un entero en un array de objetos de tipo Bit 
+     * @param decimal
+     * @return Array de objetos tipo Bit
+     */
+    private Bit[] obtenerDecimalEnBits(int decimal){
     
-    
+        String numero=Integer.toBinaryString(decimal);
+        Bit binario[]= new Bit [numero.length()];
+        
+        for (int i =0; i<numero.length();i++) {
+            if(numero.charAt(i)=='1')
+                binario[i]=new Bit(true);  
+            
+            else binario[i]=new Bit(false);
+        }
+        
+        return binario;
+    }
     public int getCuantasVeces_Horizontal(int decimal)
     {
+        Bit numero[]=obtenerDecimalEnBits(decimal);
+        
+        
         return 0;
     }
     
@@ -39,4 +60,18 @@ public class SopaBinaria {
     {
         return 0;
     }
+    
+    public String convertirDecimalABinarioManual(int decimal) {
+	if (decimal <= 0) {
+		return "0";
+	}
+	StringBuilder binario = new StringBuilder();
+	while (decimal > 0) {
+		short residuo = (short) (decimal % 2);
+		decimal = decimal / 2;
+		// Insertar el dígito al inicio de la cadena
+		binario.insert(0, String.valueOf(residuo));
+	}
+	return binario.toString();
+}
 }
