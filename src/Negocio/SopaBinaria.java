c7d3798fa4b93781825378a1599c8cb269d6639f
jeison ferrer ortega <jeisonomarfort@ufps.edu.co>
2020-10-02 21:44:58 -0500

    Busqueda horizontal completa

--- src/Negocio/SopaBinaria.java
+++ src/Negocio/SopaBinaria.java
@@ -18,18 +18,17 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
  * @author madar
  */
 public class SopaBinaria {
-    
+
     private Bit mySopaBinaria[][];
-    
-    private ArrayList<int[]> posciones ;
+
+    private ArrayList<int[]> posciones;
 
     public SopaBinaria() {
     }
-    
 
-     public SopaBinaria(String rutaArchivoExcel, int pHoja) throws IOException, Exception  {    
+    public SopaBinaria(String rutaArchivoExcel, int pHoja) throws IOException, Exception {
 
-         HSSFWorkbook archivoExcel = new HSSFWorkbook(new FileInputStream(rutaArchivoExcel));
+        HSSFWorkbook archivoExcel = new HSSFWorkbook(new FileInputStream(rutaArchivoExcel));
         //Obtiene la hoja 1
         HSSFSheet hoja = archivoExcel.getSheetAt(pHoja);
         //Obtiene el número de la última fila con datos de la hoja.
@@ -43,8 +42,6 @@ public class SopaBinaria {
             for (int j = 0; j < cantCol; j++) {
 
                 String valor = filas.getCell(j).getStringCellValue();
-                
-                
 
                 Bit nuevo = new Bit();
                 switch (valor) {
@@ -57,10 +54,9 @@ public class SopaBinaria {
                         nuevo.setValor(true);
                         this.mySopaBinaria[i][j] = nuevo;
                         break;
-                        
+
                     default:
-                        throw new Exception("Hay valores en la matriz diferentes a 0 o 1: "+"  "+valor);
-                        
+                        throw new Exception("Hay valores en la matriz diferentes a 0 o 1: " + "  " + valor);
 
                 }
 
@@ -68,137 +64,152 @@ public class SopaBinaria {
 
         }
     }
+
     /**
-     * Metodo que convierte un entero en un array de objetos de tipo Bit. 
-     * Es la representación en binario del entero ingresado.
+     * Metodo que convierte un entero en un array de objetos de tipo Bit. Es la
+     * representación en binario del entero ingresado.
+     *
      * @param decimal
      * @return Array de objetos tipo Bit
      */
-    private Bit[] obtenerDecimalEnBits(int decimal){
-    
-        String numero=Integer.toBinaryString(decimal);
-        Bit binario[]= new Bit [numero.length()];
-        
-        for (int i =0; i<numero.length();i++) {
-            if(numero.charAt(i)=='1')
-                binario[i]=new Bit(true);  
-            
-            else binario[i]=new Bit(false);
+    private Bit[] obtenerDecimalEnBits(int decimal) {
+
+        String numero = Integer.toBinaryString(decimal);
+        Bit binario[] = new Bit[numero.length()];
+
+        for (int i = 0; i < numero.length(); i++) {
+            if (numero.charAt(i) == '1') {
+                binario[i] = new Bit(true);
+            } else {
+                binario[i] = new Bit(false);
+            }
         }
-        
+
         return binario;
     }
-    public int getCuantasVeces_Horizontal(int decimal)
-    {
-        Bit numero[]=obtenerDecimalEnBits(decimal);
-        
-        boolean encontro=false;
-        
-        int contador=0;
-        
-        //Busque hacia la derecha
-        for (int[] pos : this.posciones) {
-          
-           if(pos[1]+numero.length-1 < this.mySopaBinaria[pos[0]].length){
-               int iNumero=0;
-               for(int i=pos[1];i<numero.length;i++){
-                  
-                   //System.out.println(i+" , "+iNumero+" , "+pos[0]);
-                   if(!mySopaBinaria[pos[0]][i].equals(numero[iNumero]))
-                       break;
-                   
-                   else iNumero++;
-                   
-                   if(iNumero==numero.length-1){
-                       encontro=true;
-                       System.out.println("ASASA"+pos[0]+" , "+i);
-                       contador++;
-                   }
-                   
-               }
-           }
-           
-            //i>=0
-           if(pos[1]-(numero.length-1) >=0){
-               System.out.println("***************************");
-               int iNumero=0;
-               for(int i=pos[1];i>=pos[1]-(numero.length-1);i--){
-                   if(!mySopaBinaria[pos[0]][i].equals(numero[iNumero]))
-                       break;
-                   
-                   if(iNumero==numero.length-1){
-                       encontro=true;
-                       contador++;
-                       System.out.println("Reves"+pos[0]+" , "+i);
-                   }
-                   iNumero++;
-               }
-           }
-           
+
+    public int getCuantasVeces_Horizontal(int decimal) {
+        int contador = 0;
+        switch (decimal) {
+            case 1:
+                contador = this.posciones.size();
+                break;
+            case 0:
+                contador = (mySopaBinaria.length * mySopaBinaria[0].length) - this.posciones.size();
+                break;
+            default:
+
+                Bit numero[] = obtenerDecimalEnBits(decimal);
+
+                boolean encontro = false;
+
+                //Busque hacia la derecha
+                for (int[] pos : this.posciones) {
+
+                    System.out.println("SDS: " + pos[0] + " , " + pos[1]);
+                    if (pos[1] + numero.length - 1 < this.mySopaBinaria[pos[0]].length) {
+                        int iNumero = 0;
+                        for (int i = pos[1]; i < pos[1] + numero.length; i++) {
+
+                            //System.out.println(i+" , "+iNumero+" , "+pos[0]);
+                            if (!mySopaBinaria[pos[0]][i].equals(numero[iNumero])) {
+                                break;
+                            }
+
+                            if (iNumero == numero.length - 1) {
+                                encontro = true;
+                                System.out.println("ASASA" + pos[0] + " , " + i + " , " + iNumero);
+                                contador++;
+                            }
+                            iNumero++;
+
+                        }
+                    }
+
+                    //i>=0
+                    if (pos[1] - (numero.length - 1) >= 0) {
+                        System.out.println("***************************");
+                        int iNumero = 0;
+                        for (int i = pos[1]; i >= pos[1] - (numero.length - 1); i--) {
+                            if (!mySopaBinaria[pos[0]][i].equals(numero[iNumero])) {
+                                break;
+                            }
+
+                            if (iNumero == numero.length - 1) {
+                                encontro = true;
+                                contador++;
+                                System.out.println("Reves" + pos[0] + " , " + i);
+                            }
+                            iNumero++;
+                        }
+                    }
+
+                }
+                break;
         }
-        
-        
         return contador;
     }
-    
-    
-    public int getCuantasVeces_Vertical(int decimal)
-    {
-        Bit numero[]=obtenerDecimalEnBits(decimal);
-        
-        boolean encontro=false;
-        
-        int contador=0;
-        
-        //Busque hacia la derecha
-        for (int[] pos : this.posciones) {
-          
-           if(pos[0]+numero.length-1 < this.mySopaBinaria.length){
-               int iNumero=0;
-               for(int i=pos[0];i < this.mySopaBinaria.length;i++){
-                  
-                   if(!mySopaBinaria[pos[0]][i].equals(numero[iNumero]))
-                       break;
-                   
-                   if(iNumero==numero.length-1){
-                       encontro=true;
-                       contador++;
-                   }
-                   iNumero++;
-               }
-           }
-           
-            
-           if(pos[1]-(numero.length-1) >=0){
-               System.out.println("***************************");
-               int iNumero=0;
-               for(int i=pos[1];i>=0;i--){
-                   if(!mySopaBinaria[pos[0]][i].equals(numero[iNumero]))
-                       break;
-                   
-                   if(iNumero==numero.length-1){
-                       encontro=true;
-                       contador++;
-                   }
-                   iNumero++;
-               }
-           }
-           
+
+    public int getCuantasVeces_Vertical(int decimal) {
+
+        int contador = 0;
+
+        switch (decimal) {
+            case 1:
+                contador = this.posciones.size();
+                break;
+            case 0:
+                contador = (mySopaBinaria.length * mySopaBinaria[0].length) - this.posciones.size();
+                break;
+            default:
+                Bit numero[] = obtenerDecimalEnBits(decimal);
+                boolean encontro = false;
+                //Busque hacia la derecha
+                for (int[] pos : this.posciones) {
+
+                    if (pos[0] + numero.length - 1 < this.mySopaBinaria.length) {
+                        int iNumero = 0;
+                        for (int i = pos[0]; i < this.mySopaBinaria.length; i++) {
+
+                            if (!mySopaBinaria[pos[0]][i].equals(numero[iNumero])) {
+                                break;
+                            }
+
+                            if (iNumero == numero.length - 1) {
+                                encontro = true;
+                                contador++;
+                            }
+                            iNumero++;
+                        }
+                    }
+
+                    if (pos[1] - (numero.length - 1) >= 0) {
+                        System.out.println("***************************");
+                        int iNumero = 0;
+                        for (int i = pos[1]; i >= 0; i--) {
+                            if (!mySopaBinaria[pos[0]][i].equals(numero[iNumero])) {
+                                break;
+                            }
+
+                            if (iNumero == numero.length - 1) {
+                                encontro = true;
+                                contador++;
+                            }
+                            iNumero++;
+                        }
+                    }
+
+                }
+                break;
         }
-        
-        
         return contador;
     }
-    
-    
-    
-    public int getCuantasVeces_Diagonal(int decimal)
-    {
+
+    public int getCuantasVeces_Diagonal(int decimal) {
         return 0;
     }
-    
-    
-     public String[][] getMatrizEnString() {
+
+    public String[][] getMatrizEnString() {
 
         String matrizEnString[][] = new String[this.mySopaBinaria.length][];
         String valor = "0";
@@ -221,76 +232,73 @@ public class SopaBinaria {
         }
         return matrizEnString;
     }
-    
+
     public Bit[][] getMatriz() {
         return this.mySopaBinaria;
     }
-    public String toString()
-    {
-    String msg="";
-        for( Bit filas[]:this.mySopaBinaria)
-        {
-            for(Bit valor: filas)
-                msg+=valor.toString()+"\t";
-        msg+="\n";
+
+    public String toString() {
+        String msg = "";
+        for (Bit filas[] : this.mySopaBinaria) {
+            for (Bit valor : filas) {
+                msg += valor.toString() + "\t";
+            }
+            msg += "\n";
         }
-        
-    return msg;
-    }
-    
-    public String buscar(int decimal){
-    
-        System.out.println(toString());
-        String msg="";
-        String decimalBinario ="";
-        Bit numero[]= this.obtenerDecimalEnBits(decimal);
-        
-        buscarUnos();
-        
-        
-        for (Bit bit : numero) {
-            decimalBinario+=bit.toString();
-        }
-        int cuantasVecesTotal=this.getCuantasVeces_Diagonal(decimal)+
-                this.getCuantasVeces_Horizontal(decimal);
-        msg="Se econtro el numero decimal "+decimal+" en binario: "+decimalBinario+
-                " "+cuantasVecesTotal+" veces en la sopa binaria.";
+
         return msg;
-    
     }
-    
-    private void buscarUnos(){
-    
-    this.posciones = new ArrayList <int[]>();
-        
+
+    public String buscar(int decimal) {
+
+        System.out.println(toString());
+        String msg = "";
+        String decimalBinario = "";
+        Bit numero[] = this.obtenerDecimalEnBits(decimal);
+
+        buscarUnos();
+
+        for (Bit bit : numero) {
+            decimalBinario += bit.toString();
+        }
+
+        int cuantasVecesTotal = this.getCuantasVeces_Diagonal(decimal)
+                + this.getCuantasVeces_Horizontal(decimal);
+        msg = "Se econtro el numero decimal " + decimal + " en binario: " + decimalBinario
+                + " " + cuantasVecesTotal + " veces en la sopa binaria.";
+        return msg;
+
+    }
+
+    private void buscarUnos() {
+
+        this.posciones = new ArrayList<int[]>();
+
         for (int i = 0; i < this.mySopaBinaria.length; i++) {
             for (int j = 0; j < this.mySopaBinaria[i].length; j++) {
-                
-                int pos []= new int [2];
-                pos[0]=i;
-                pos[1]=j;
-                
+
+                int pos[] = new int[2];
+                pos[0] = i;
+                pos[1] = j;
+
                 //System.out.println(pos[0]+","+pos[1]);
-                if(this.mySopaBinaria[i][j].isValor()){
+                if (this.mySopaBinaria[i][j].isValor()) {
                     this.posciones.add(pos);
                 }
                 //System.out.println(this.mySopaBinaria[i][j].isValor());
-                
-            }
-            
-        }
-        
-    }
-    
-    private void buscarUnosPrueba(){
-        for (int i =0;i<this.posciones.size();i++) {
-            int pos[]=this.posciones.get(i);
-            System.out.println(pos[0]+","+pos[1]);
-            
-            
-        }
-    }
-            
 
-   
+            }
+
+        }
+
+    }
+
+    private void buscarUnosPrueba() {
+        for (int i = 0; i < this.posciones.size(); i++) {
+            int pos[] = this.posciones.get(i);
+            System.out.println(pos[0] + "," + pos[1]);
+
+        }
+    }
+
 }
